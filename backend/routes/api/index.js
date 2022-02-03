736c4e167971e81af13adadc0d513b999035007f
Chiew <chiews89@gmail.com>
2022-02-03 10:55:27 -0800

    login route done

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,7 +2,18 @@ const router = require("express").Router();
 const asyncHandler = require("express-async-handler");
 const { setTokenCookie } = require("../../utils/auth.js");
 const { User } = require("../../db/models");
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+module.exports = router;
 
 
 module.exports = router;
