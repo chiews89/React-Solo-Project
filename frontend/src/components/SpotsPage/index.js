ced2c9d1562823fb66f33f24832384040f3c0610
Chiew <chiews89@gmail.com>
2022-02-08 13:29:41 -0800

    all spots being displayed properly

--- frontend/src/components/SpotsPage/index.js
+++ frontend/src/components/SpotsPage/index.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getSpots } from "../../store/spots";
 
@@ -6,19 +7,43 @@ const AllSpots = () => {
   const dispatch = useDispatch();
 
   const spots = useSelector((state) => {
-    return Object.values(state.spots);
+    return state.spots;
   });
+  const spotsObj = Object.values(spots);
 
   useEffect(() => {
     dispatch(getSpots());
   }, [dispatch]);
 
   return (
-  <>
-  <div>
-    
-  </div>
-  </>);
+    <>
+      {spotsObj.map((spot) => (
+        <div key={spot.id}>
+          <NavLink to={`/spots/${spot.id}`}>
+            <img
+              alt={spot.name}
+              src={
+                spot.Images[0]
+                  ? spot.Images[0].url
+                  : "https://st4.depositphotos.com/14953852/22772/v/600/depositphotos_227725020-stock-illustration-image-available-icon-flat-vector.jpg"
+              }
+            />
+          </NavLink>
+            <p>{spot.description}</p>
+          <div>
+            Location: {spot.city}, {spot.state}
+          </div>
+          <div>
+            Price:{" "}
+            {`$${spot.price
+              .toString()
+              .replace(/\B(=(\d{3})+(!\d))/g, ",")} / day`}
+          </div>
+
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default AllSpots;
