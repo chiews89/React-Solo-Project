46265ee6833c8792953863840a8064c2fb502c2d
Chiew <chiews89@gmail.com>
2022-06-22 11:31:44 -0700

    changed moment to work on live

--- frontend/src/components/UserProfile/UserPast/UserPast.js
+++ frontend/src/components/UserProfile/UserPast/UserPast.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import moment from "react-moment";
+import moment from "moment";
 
 export const UserPast = () => {
   const user = useSelector((state) => state?.session?.user);
@@ -19,8 +19,9 @@ export const UserPast = () => {
   }
 
   const noImage = (e) => {
-    e.target.src = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
-  }
+    e.target.src =
+      "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg";
+  };
 
   return (
     <div className="profile-page-container">
@@ -44,7 +45,7 @@ export const UserPast = () => {
           <h2>Your Previous Trips</h2>
         </div>
         <div className="no-booking">
-        <NavLink to={`/spots`}>Find your next trip</NavLink>
+          <NavLink to={`/spots`}>Find your next trip</NavLink>
         </div>
       </div>
       <div className="user-listings-container">
@@ -64,17 +65,31 @@ export const UserPast = () => {
               </NavLink>
             </div>
             <div className="profile-booking-dates">
-                                    <div>
-                                        <span>Check In {moment(booking.checkIn).add('days', 1).format('MMMM D YYYY')}</span>
-                                    </div>
-                                    <div>
-                                        <span>Check Out {moment(booking.checkOut).add('days', 1).format('MMMM D YYYY')}</span>
-                                    </div>
-                                </div>
+              <div>
+                <span>
+                  Check In{" "}
+                  {moment(booking.checkIn).add("days", 1).format("MMMM D YYYY")}
+                </span>
+              </div>
+              <div>
+                <span>
+                  Check Out{" "}
+                  {moment(booking.checkOut)
+                    .add("days", 1)
+                    .format("MMMM D YYYY")}
+                </span>
+              </div>
+            </div>
             <div className="profile-spot-address">
-              <span className="profile-spot-address-info">{booking?.Spot?.address}</span>
-              <span className="profile-spot-address-info">{booking?.Spot?.city}</span>
-              <span className="profile-spot-address-info">{booking?.Spot?.state}</span>
+              <span className="profile-spot-address-info">
+                {booking?.Spot?.address}
+              </span>
+              <span className="profile-spot-address-info">
+                {booking?.Spot?.city}
+              </span>
+              <span className="profile-spot-address-info">
+                {booking?.Spot?.state}
+              </span>
             </div>
           </div>
         ))}
--- frontend/src/components/UserProfile/UserUpcoming/UserUpcoming.js
+++ frontend/src/components/UserProfile/UserUpcoming/UserUpcoming.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import moment from "react-moment";
+import moment from "moment";
 import { DeleteBooking } from "../../Booking/DeleteBooking/DeleteBooking";
 
 export const UserUpcoming = () => {
   const user = useSelector((state) => state?.session?.user);
-  const bookings = Object.values(useSelector((state) => state?.bookings)).reverse();
+  const bookings = Object.values(
+    useSelector((state) => state?.bookings)
+  ).reverse();
   const userBookings = bookings.filter((booking) => {
     return booking.userId === user?.id;
   });
   const upcomingBookings = userBookings.filter(
-    (upcoming) => moment(upcoming.checkIn) > moment()
+    upcoming => moment(upcoming.checkIn) > moment()
   );
 
   if (!user) {
@@ -19,8 +21,9 @@ export const UserUpcoming = () => {
   }
 
   const noImage = (e) => {
-    e.target.src = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
-  }
+    e.target.src =
+      "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg";
+  };
 
   return (
     <div className="profile-page-container">
@@ -67,20 +70,35 @@ export const UserUpcoming = () => {
               </NavLink>
             </div>
             <div className="profile-booking-dates">
-                                    <div>
-                                        <span>Check In {moment(booking.checkIn).add('days', 1).format('MMMM D YYYY')}</span>
-                                    </div>
-                                    <div>
-                                        <span>Check Out {moment(booking.checkOut).add('days', 1).format('MMMM D YYYY')}</span>
-                                    </div>
-                                </div>
+              <div>
+                <span>
+                  Check In{" "}
+                  {moment(booking.checkIn).add("days", 1).format("MMMM D YYYY")}
+                </span>
+              </div>
+              <div>
+                <span>
+                  Check Out{" "}
+                  {moment(booking.checkOut)
+                    .add("days", 1)
+                    .format("MMMM D YYYY")}
+                </span>
+              </div>
+            </div>
             <div className="profile-spot-address">
-
-              <span className="profile-spot-address-info">{booking?.Spot?.address}
-              <span> <DeleteBooking booking={booking.id} /></span>
+              <span className="profile-spot-address-info">
+                {booking?.Spot?.address}
+                <span>
+                  {" "}
+                  <DeleteBooking booking={booking.id} />
+                </span>
               </span>
-              <span className="profile-spot-address-info">{booking?.Spot?.city}</span>
-              <span className="profile-spot-address-info">{booking?.Spot?.state}</span>
+              <span className="profile-spot-address-info">
+                {booking?.Spot?.city}
+              </span>
+              <span className="profile-spot-address-info">
+                {booking?.Spot?.state}
+              </span>
             </div>
           </div>
         ))}
