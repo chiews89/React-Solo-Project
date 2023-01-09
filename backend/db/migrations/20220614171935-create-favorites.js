0a29702a8392acac25e82af9bb10cc3705f7d9e4
Chiew <chiews89@gmail.com>
2023-01-09 14:12:22 -0800

    changing to render

--- backend/db/migrations/20220614171935-create-favorites.js
+++ backend/db/migrations/20220614171935-create-favorites.js
@@ -1,4 +1,10 @@
 "use strict";
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable("Favorites", {
@@ -21,15 +27,15 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+        type: Sequelize.DATE,
+      },
+    }, options);
   },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable("Favorites");
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Favorites", options);
   },
 };
--- backend/db/seeders/20220203175937-demo-user.js
+++ backend/db/seeders/20220203175937-demo-user.js
@@ -1,6 +1,11 @@
 "use strict";
 const bcrypt = require("bcryptjs");
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert(
